4ad78ec2242b799c30b0cea023f9411a4e4adc17
Sabrin Kater <sabrynkhatr696@gmail.com>
2026-08-11 21:56:53 +0300

    Add files via upload

--- utils/translations.py
+++ utils/translations.py
@@ -1,14 +1,8 @@
 import streamlit as st
 
 def init_language():
-    """تثبيت اللغة الإنجليزية بشكل دائم وإظهار اللوجو في السايدبار"""
+    """تثبيت اللغة الإنجليزية بشكل دائم"""
     st.session_state["lang"] = "en"
-    
-    # إظهار اللوجو في أعلى القائمة الجانبية (Sidebar) لكل الصفحات
-    try:
-        st.sidebar.image("assets/logo.png", use_container_width=True)
-    except Exception:
-        pass
 
 def t(key: str) -> str:
     """إرجاع مفتاح النص الأصلي كما هو باللغة الإنجليزية"""
